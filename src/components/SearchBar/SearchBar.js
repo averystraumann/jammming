15db0f74af827e4e06245c182a27db5e14bcccf3
Avery Straumann <averystraumann@icloud.com>
2024-07-27 01:05:24 -0400

    searchg bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 
 export default function SearchBar(props) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleClick() {
+    alert(`you searched for ${searchTerm}`);
+  }
+
   return (
-    <form>
+    <div className="searchBar">
       <label for="search-input">Search</label>
       <input
         type="search"
         name="search-input"
         id="search-input"
         placeholder="Search for Music"
-        onChange={props.setSearchInput}
+        onChange={(e) => setSearchTerm(e.target.value)}
       ></input>
+      <p>{searchTerm}</p>
 
-      <p>{searchInput}</p>
-
-      <input type="submit"></input>
-    </form>
+      <button onClick={handleClick}>GO!</button>
+    </div>
   );
 }
